ce9015c850c78f8d88905a48ce4e6f668a2966da
wukongexploit <wukongexploit@gmail.com>
2026-08-14 00:23:05 +0700

    Update teameye-alfa.php

--- teameye-alfa.php
+++ teameye-alfa.php
@@ -1119,7 +1119,7 @@ echo '<input type="hidden" name="charset">
 <tr>
 <td><span class="header_vars">User:</span></td>
 <td><span class="header_values" id="header_userid">'. $uid . ' [ ' . $user . ' ] </span><span class="header_vars"> Group: </span><span class="header_values" id="header_groupid">' . $gid . ' [ ' . $group . ' ]</span> </td>
-<td width="12%" rowspan="8"><img style="border-radius:100px;" width="200" height="170" alt="Wukong_Exploit" draggable="false" src="https://ik.imagekit.io/n8ym6nf86/3e6a0632-70ee-469b-ba18-4a9b9ab510d2.jfif" /></td>
+<td width="12%" rowspan="8"><img style="border-radius:100px;" width="200" height="170" alt="Wukong_Exploit" draggable="false" src="https://ik.imagekit.io/n8ym6nf86/montana.png" /></td>
 </tr>
 <tr>
 <td><span class="header_vars">PHP:</span></td>
