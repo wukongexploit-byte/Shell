2a89e1fa506f867a4623587aa011af5352f9af48
wukongexploit <wukongexploit@gmail.com>
2026-09-14 16:59:18 +0700

    Update user image source in teameye-alfa.php

--- teameye-alfa.php
+++ teameye-alfa.php
@@ -1119,7 +1119,7 @@ echo '<input type="hidden" name="charset">
 <tr>
 <td><span class="header_vars">User:</span></td>
 <td><span class="header_values" id="header_userid">'. $uid . ' [ ' . $user . ' ] </span><span class="header_vars"> Group: </span><span class="header_values" id="header_groupid">' . $gid . ' [ ' . $group . ' ]</span> </td>
-<td width="12%" rowspan="8"><img style="border-radius:100px;" width="200" height="170" alt="Wukong_Exploit" draggable="false" src="https://ik.imagekit.io/n8ym6nf86/montana.png" /></td>
+<td width="12%" rowspan="8"><img style="border-radius:100px;" width="200" height="170" alt="Wukong_Exploit" draggable="false" src="https://t4.ftcdn.net/jpg/05/90/89/33/240_F_590893393_4q8HbhLFMiVVUSPCg3pcfyoBe1xpDpDT.jpg" /></td>
 </tr>
 <tr>
 <td><span class="header_vars">PHP:</span></td>
